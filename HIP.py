4736c24d6975307f9e6ad25d67444fb5cb8ae8d0
hamedkiani <hamedkg@gmail.com>
2018-11-22 19:27:15 -0500

    some random test

---
+++ HIP.py
@@ -0,0 +1,11 @@
+import numpy as np
+import os
+
+class HIP:
+    def __init__(self):
+        self.log = "a HIP is generated!"
+
+    def print_log(self):
+        print(self.txt)
+
+
---
+++ HIP_demo.py
@@ -0,0 +1,3 @@
+from HIP import *
+
+
